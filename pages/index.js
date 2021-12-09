f9caf1ab6b96a493e7b7e70c05eb98a3c7508f58
Marley Kanui <marley.kanui@gmail.com>
2021-12-09 07:49:07 -0600

    iframe text around

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Home() {
       <Head>
         <script src="//cdn.8thwall.com/web/iframe/iframe.js"></script>
       </Head>
+
       <div
         style={{
           position: "fixed",
@@ -24,17 +25,38 @@ export default function Home() {
           left: "0",
           width: "100vw",
           height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          // display: "grid",
+          // gridAutoColumns: "1fr",
+          // gridAutoFlow: "column",
         }}
       >
+        <div>
+          Commodo laboris veniam duis id aute duis esse fugiat incididunt mollit
+          nostrud. Amet aliquip ipsum excepteur occaecat. Nulla esse nulla
+          nostrud incididunt. Dolore do culpa esse in sunt aute proident sit.
+          Labore consectetur elit ad in Lorem tempor aliqua non qui in. Dolor
+          proident ea aute magna nostrud sunt officia laborum culpa officia.
+          Anim mollit Lorem elit pariatur tempor et dolore nulla sint. Eu non
+          est magna qui cillum laboris dolor incididunt nulla nostrud ad ea.
+          Elit laborum adipisicing ut id non. Proident consequat enim magna sunt
+          nostrud ut et velit ipsum ipsum fugiat deserunt.
+        </div>
         <iframe
           id="my-iframe"
-          style={{ height: "50%", width: "50%" }}
+          style={{ width: "90%", height: "90%" }}
           allow="camera;microphone;gyroscope;accelerometer;"
           src="https://d2odbxwfe3i8mn.cloudfront.net/"
         ></iframe>
+        <div>
+          Magna ut aute mollit non exercitation exercitation ad. Veniam anim sit
+          officia reprehenderit occaecat non enim. Elit duis est occaecat labore
+          amet aute. Reprehenderit nostrud dolore sunt officia et est occaecat.
+          Excepteur officia fugiat amet nulla aliquip amet Lorem cupidatat magna
+          ut aute culpa. Veniam enim mollit proident mollit minim adipisicing
+          adipisicing in anim enim ad veniam exercitation ullamco. Occaecat
+          culpa irure laboris quis minim culpa sit tempor reprehenderit culpa
+          aute. Sint in pariatur aute cillum voluptate.
+        </div>
       </div>
     </div>
   );
